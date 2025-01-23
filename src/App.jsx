ed03ab55a21de5bdcd14765a8b605f8ca8d9d47c
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-01-23 12:34:06 +0530

    main changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,30 @@ import { useEffect } from 'react'
 import './App.css'
 
 
-todoList = [{
-          id : 1,
-          todo : 'go to gym',
-          descriptipn : "go gym at evening"
-        },
-        {
-          id : 2,
-          todo : "eat food",
-          descriptipn : 'Briyani'
-        },
-      {
-        id : 3, 
-        todo : 'Study',
-        descriptipn: 'Study the Cohort'
-      }]
+// todoList = [{
+//           id : 1,
+//           todo : 'go to gym',
+//           descriptipn : "go gym at evening"
+//         },
+//         {
+//           id : 2,
+//           todo : "eat food",
+//           descriptipn : 'Briyani'
+//         },
+//       {
+//         id = 3, 
+//         todo : 'Study',
+//         descriptipn: 'Study the Cohort'
+//       }]
 
 function App() {
   const [count, setCount] = useState(0)
 
-  // useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setCount(count => count + 1);
-  //   }, 1000)
-  // }, [count])
-
   useEffect(()=>{
-    
-  })
+    setTimeout(()=>{
+      setCount(count => count + 1);
+    }, 1000)
+  }, [count])
 
   //wihout dependecies callback function will call each time when component state changes
   //with dependcies it will be only 1 time when state changes or page reload
@@ -41,7 +37,7 @@ function App() {
       <button>Add 1</button>
       <button>Add 2</button>
       <button>Add 3</button>
-      <h2>I will be change in every 1 second :{count}</h2>
+      <h2>I will be change in every 1 second : <button>{count}</button></h2>
     </>
   )
 }
